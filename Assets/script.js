0f78c8468d1da3714bea1edce51f5e000f6bfe1b
connorbodin <connor.e.bodin@gmail.com>
2023-01-08 16:09:33 -0600

    Initial code for challenge 4, CSS file, JS file, and Index!

---
+++ Assets/script.js
@@ -0,0 +1,39 @@
+// when next or start game move to hidden and bring in buttons from being hidding 
+
+// Java time! 
+
+
+
+const myQuestions = [
+    {
+      question: "What is 1+1?",
+      answers: {
+        a: "2",
+        b: "3",
+        c: "4",
+        d: '77'
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What is 2+2?",
+      answers: {
+        a: "7",
+        b: "8",
+        c: "9",
+        d: "4"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "What is 5+5",
+      answers: {
+        a: "6",
+        b: "8",
+        c: "10",
+        d: "15"
+      },
+      correctAnswer: "c"
+    }
+  ];
+

5b165ace27da01c8cf50caa047a60846dbca9533
connorbodin <connor.e.bodin@gmail.com>
2023-01-19 18:51:21 -0600

    Changed things a lot, went with 3 html pages instead of one and am watching a great youtube tutorial on quiz generators

--- Assets/script.js
+++ Assets/script.js
@@ -1,29 +1,74 @@
-let startButton = document.getElementById("start-btn");
-let quizContainer = document.getElementById("hide");
+var question = document.getElementById('question');
+var choices = Array.from(document.getElementsByClassName('choice-text'));
 
-console.log(startButton);
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 
 
+let questions = [
+    {
+        question: "What is 1+1?",
+        choice1: "32",
+        choice2: "2",
+        choice3: "4",
+        choice4: "6",
+        answer: 2
 
-console.log(quizContainer);
+    },
+    {
+        question: "What is 1+1?",
+        choice1: "32",
+        choice2: "2",
+        choice3: "4",
+        choice4: "6",
+        answer: 2
 
+    },
+    {
+        question: "What is 1+1?",
+        choice1: "32",
+        choice2: "2",
+        choice3: "4",
+        choice4: "6",
+        answer: 2
 
-startButton.addEventListener('click', function(){
-    showOrHideDiv();
-});
-
-function showOrHideDiv() {
-    var v = document.getElementById("start-container");
-    if (v.style.display === "none") {
-       v.style.display = "block";
-    } else {
-       v.style.display = "none";
-       quizContainer.style.display = "flex";
+    },
+    {
+        question: "What is 1+1?",
+        choice1: "32",
+        choice2: "2",
+        choice3: "4",
+        choice4: "6",
+        answer: 2
+    },
+    {
+        question: "What is 1+1?",
+        choice1: "32",
+        choice2: "2",
+        choice3: "4",
+        choice4: "6",
+        answer: 2
     }
-};
+]
 
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 5;
 
+function startGame() {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [ ... questions];
+    getNextQuestion();
+}
 
+function getNextQuestion() {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+}
 
-
-
+startGame();

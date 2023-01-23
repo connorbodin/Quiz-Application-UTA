0912ef9521b5bc7a2772ddf209efedeb6794319d
connorbodin <connor.e.bodin@gmail.com>
2023-01-22 20:19:40 -0600

    added timer to subtract 5 if you get the question wrong and add 5 if you get it correctly

--- Assets/script.js
+++ Assets/script.js
@@ -1,119 +1,118 @@
-var question = document.getElementById('question');
-var choices = Array.from(document.getElementsByClassName('choice-text'));
+var question = document.getElementById("question");
+var choices = Array.from(document.getElementsByClassName("choice-text"));
 console.log(choices);
 let currentQuestion = {};
 let acceptingAnswers = true;
-let secondsLeft = 15; 
+let secondsLeft = 30;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 let timeEl = document.getElementById("timer");
 
-
 let questions = [
-    {
-        question: "What is 1+1?",
-        choice1: "32",
-        choice2: "2",
-        choice3: "4",
-        choice4: "6",
-        answer: 2
-
-    },
-    {
-        question: "What is 2+1?",
-        choice1: "32",
-        choice2: "2",
-        choice3: "4",
-        choice4: "6",
-        answer: 2
-
-    },
-    {
-        question: "What is 3+1?",
-        choice1: "32",
-        choice2: "2",
-        choice3: "4",
-        choice4: "6",
-        answer: 2
-
-    },
-    {
-        question: "What is 4+1?",
-        choice1: "32",
-        choice2: "2",
-        choice3: "4",
-        choice4: "6",
-        answer: 2
-    },
-    {
-        question: "What is 5+1?",
-        choice1: "32",
-        choice2: "2",
-        choice3: "4",
-        choice4: "6",
-        answer: 2
-    }
-]
+  {
+    question: "What is 1+1?",
+    choice1: "32",
+    choice2: "2",
+    choice3: "4",
+    choice4: "6",
+    answer: 2,
+  },
+  {
+    question: "What is 2+1?",
+    choice1: "32",
+    choice2: "2",
+    choice3: "4",
+    choice4: "6",
+    answer: 2,
+  },
+  {
+    question: "What is 3+1?",
+    choice1: "32",
+    choice2: "2",
+    choice3: "4",
+    choice4: "6",
+    answer: 2,
+  },
+  {
+    question: "What is 4+1?",
+    choice1: "32",
+    choice2: "2",
+    choice3: "4",
+    choice4: "6",
+    answer: 2,
+  },
+  {
+    question: "What is 5+1?",
+    choice1: "32",
+    choice2: "2",
+    choice3: "4",
+    choice4: "6",
+    answer: 2,
+  },
+];
 
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 5;
 
 function startGame() {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [ ... questions];
-    console.log(availableQuestions);
-    getNextQuestion();
-    setTime();
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  console.log(availableQuestions);
+  getNextQuestion();
+  setTime();
 }
 
 function getNextQuestion() {
-    questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+// Assigns the options choices to the page
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
 
-    choices.forEach( choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
+  availableQuestions.splice(questionIndex, 1);
 
-    availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+}
 
-    acceptingAnswers = true;
-};
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+    console.log(e.target);
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswers = selectedChoice.dataset["number"];
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return;
-        console.log(e.target);
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswers = selectedChoice.dataset["number"];
+    // learned the += and -= from freecodecamp.org
+    console.log(selectedAnswers == currentQuestion.answer);
+    if (selectedAnswers == currentQuestion.answer) {
+      secondsLeft += 5;
+    } else {
+      secondsLeft -= 5;
+    }
 
-        console.log(selectedAnswers);
+    getNextQuestion();
 
-
-        getNextQuestion();
-    });
+  });
 });
 
 function setTime() {
-    // Sets interval in variable
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.innerHTML = '⏲' + secondsLeft
-  
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Calls function to create and append image
-      }
-  
-    }, 1000);
-  }
+  // Sets interval in variable
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.innerHTML = "⏲" + secondsLeft;
 
-
+    if (secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+    }
+  }, 1000);
+}
 
 startGame();
